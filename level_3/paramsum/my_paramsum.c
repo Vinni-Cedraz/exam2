61af0a02948f951be3d783d6964dec82d2538006
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-03-23 19:51:57 -0300

    paramsum

---
+++ level_3/paramsum/my_paramsum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+static inline int my_putchar(char c) {
+    write(1, &c, 1);
+    return 0;
+}
+
+static inline void my_putnbr(int nbr) {
+    if (nbr > 9) my_putnbr(nbr / 10);
+    my_putchar((nbr % 10) + '0');
+}
+
+int main(int argc, char **argv) {
+    (void)argv;
+    my_putnbr(argc - 1);
+	my_putchar('\n');
+}
