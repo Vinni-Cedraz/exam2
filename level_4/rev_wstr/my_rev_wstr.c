a3c89e69c3097bb3c9f9e21f24d7f20d77567d4f
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-03-24 20:50:02 -0300

    my_rev_wstr bug fix

--- level_4/rev_wstr/my_rev_wstr.c
+++ level_4/rev_wstr/my_rev_wstr.c
@@ -6,9 +6,9 @@ static char *next_word(char *s) {
     return (s + 1);
 }
 
-static void my_putnstr(char *str, int n) {
-    while (n--) write(1, str++, 1);
-    write(1, " ", 1);
+static void my_putnstr(char *str, int n, int is_last_word) {
+    while (n-- && *str) write(1, str++, 1);
+    if (!is_last_word) write(1, " ", 1);
 }
 
 static char my_strlen(char *str) {
@@ -17,13 +17,16 @@ static char my_strlen(char *str) {
     return end - str;
 }
 
-static void create_str_stack(char *s) {
-    if (next_word(s)) create_str_stack(next_word(s));
-    my_putnstr(s, my_strlen(s));
+static void my_rev_wstr(char *s, int is_last_word) {
+    char *next = next_word(s);
+    if (next) my_rev_wstr(next, 0);
+    my_putnstr(s, my_strlen(s), is_last_word);
 }
 
+// remember recursion is LIFO, so the first word in av[1] is gonna be put last,
+// that's why first call has the is_last_word flag set to true
 int main(int ac, char **av) {
     if (ac != 2) return (write(1, "\n", 1));
-    create_str_stack(av[1]);
+    my_rev_wstr(av[1], 1);
     write(1, "\n", 1);
 }
