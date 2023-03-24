0673f9d925aa5842aea010eced263b52f3e9a2da
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-03-24 18:47:12 -0300

    refactored with miniatoi

--- level_3/add_prime_sum/my_add_prime_sum.c
+++ level_3/add_prime_sum/my_add_prime_sum.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 
-static inline int is_prime(int nb);
-static inline void my_putnbr(int nb);
-static inline void my_putchar(char c) { write(1, &c, 1); };
-static inline int simple_atoi(char *str);
+static int is_prime(int nb);
+static void my_putnbr(int nb);
+static void my_putchar(char c) { write(1, &c, 1); };
+static int miniatoi(char *a, int toi);
 
 int main(int argc, char *argv[]) {
     int i = -1;
     int sum = 0;
-	int num = simple_atoi(argv[1]);
+    int num = miniatoi(argv[1], 0);
     if (argc != 2 || num < 1) my_putnbr(0), my_putchar('\n');
     while (++i <= num)
         if (is_prime(i)) sum += i;
@@ -17,29 +17,18 @@ int main(int argc, char *argv[]) {
     my_putchar('\n');
 }
 
-static inline int my_strlen(char *str){
-	char *end = str;
-	while (end && *end) end++;
-	return end - str;
+static int miniatoi(char *a, int toi) {
+    if (!*a) return toi;
+    toi = toi * 10 + *a - '0';
+    return miniatoi(a + 1, toi);
 }
 
-static inline int simple_atoi(char *str) {
-	int toi, zeros, len;
-	toi = 0, zeros = 1, len = my_strlen(str);
-
-	while (len--) {
-		toi += (str[len] - '0') * zeros;
-		zeros *= 10;
-	}
-	return toi;
-}
-
-static inline void my_putnbr(int nb) {
+static void my_putnbr(int nb) {
     if (nb > 9) my_putnbr(nb / 10);
     my_putchar(nb % 10 + '0');
 }
 
-static inline int is_prime(int nb) {
+static int is_prime(int nb) {
     int i = 2;
     if (nb == 1) return (0);
     while (i <= nb / 2) {
