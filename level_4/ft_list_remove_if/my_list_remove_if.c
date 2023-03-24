f46d035399fa3ccf3cb6b8dcac5029b63e386dd9
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-03-24 16:41:15 -0300

    concise my_list_remove_if, iterative and recursive

--- level_4/ft_list_remove_if/my_list_remove_if.c
+++ level_4/ft_list_remove_if/my_list_remove_if.c
@@ -1,24 +1,43 @@
 #include <stdlib.h>
+
+// notice that the recursive function didnt require the creating of a second
+// double pointer to the beginig of the list. This is because at the end of the
+// recursive calls the initial address is preserved so reference isnt lost.
+
 #include "ft_list.h"
 
-void my_list_remove_if(t_list **begin_list, void *data_ref,
-                       int (*cmp)(void *, void *)) {
-    t_list *current = *begin_list;
-    t_list *prev = NULL;
-    while (current) {
-        if (!cmp(current->data, data_ref)) {
-            if (prev) {
-                prev->next = current->next;
-                free(current);
-                current = prev->next;
-            } else {
-                *begin_list = current->next;
-                free(current);
-                current = *begin_list;
-            }
-        } else {
-            prev = current;
-            current = current->next;
-        }
-    }
+typedef void *x;
+
+// // ==========================RECURSIVE APPROACH===========================//
+
+void my_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(x, x)) {
+    if (!*begin_list) return;
+    if (!(*cmp)((*begin_list)->data, data_ref)) {
+        t_list *tmp = *begin_list;
+        *begin_list = (*begin_list)->next;
+        free(tmp);
+        my_list_remove_if(begin_list, data_ref, cmp);
+    } else
+        my_list_remove_if(&(*begin_list)->next, data_ref, cmp);
 }
+
+// // =========================ITERATIVE APPROACH============================//
+
+// on the iterative approach we need a third variable that will point to the
+// original list so that we can walk through the list without changing it. But
+// we can also change the original list when necessary (that is, when removing a
+// node); To change the original we derefence this third variable and to walk
+// without changing the original we dont derefence.
+
+// void my_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(x, x))
+// {
+//     t_list **curr = begin_list;
+//     while (*curr) {
+//         if (!(*cmp)((*curr)->data, data_ref)) {
+//             t_list *to_rmv = *curr;
+//             *curr = (*curr)->next;
+//             free(to_rmv);
+//         } else
+//             curr = &(*curr)->next;
+//     }
+// }
