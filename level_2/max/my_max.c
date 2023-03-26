6288ece103b7a4779c3a84e8e7db3bbbb5db5dcd
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-03-26 13:33:35 -0300

    improved test for my max

--- level_2/max/my_max.c
+++ level_2/max/my_max.c
@@ -1,33 +1,25 @@
 int my_max(int *tab, unsigned int len) {
-  int max = 0;
-  while (tab && len--) {
-    if (*tab > max)
-      max = *tab;
-    tab++;
-  }
-  return (max);
+    int max = 0;
+    while (tab && len--) {
+        if (*tab > max) max = *tab;
+        tab++;
+    }
+    return (max);
 }
 
-
-// #include <math.h>
 // #include <stdio.h>
 // #include <stdlib.h>
-// #include <strings.h>
-//
+// #include <time.h>
 // int main() {
-//   int j = 0;
-//   static uint i = 5;
-//   int tab[99];
+//     int j = -1;
+// 	int len = 119;
+//     int tab[len];
 //
-//   while (j < 99) {
-//     i = (i >> 1) | (i << 31) ^ ((i >> 1) | (i << 31) ^ (i << 1));
-//     tab[j] = i % 100;
-//     j++;
-//   }
-//   while (j--)
-//     printf("%d ", tab[j]);
-//   printf("\n");
-//   printf("\n");
-//   int *empty = NULL;
-//   printf("%d\n", my_max(tab, 45));
+// 	srand(time(NULL));
+// 	while (++j < len) tab[j++] = random() % len;
+//     j = -1;
+//     while (++j < len) printf("%d ", tab[j]);
+//     printf("\n");
+//     printf("\n");
+//     printf("%d\n", my_max(tab, len));
 // }
