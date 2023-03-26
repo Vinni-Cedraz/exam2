79aafb95f5036d7c9984d092d3495bf4a90563d5
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-03-26 11:56:18 -0300

    simpler strspn

--- level_2/ft_strspn/my_strspn.c
+++ level_2/ft_strspn/my_strspn.c
@@ -1,37 +1,33 @@
-#include <assert.h>
 #include <stddef.h>
-#include <stdio.h>
-#include <string.h>
 
-int how_many(const char **s, char c) {
-  int len = 0;
-  while (**s == c) {
-    len++;
-    (*s)++;
-  }
-  return len;
+int char_is_there(const char *s, int c) {
+    while (*s) {
+        if (*s == c) return 1;
+        ++s;
+    }
+    return (0);
 }
 
 size_t my_strspn(const char *s, const char *accept) {
-  int len = 0;
-  if (!s || !accept)
-    return 0;
-  while (*s && *accept) {
-    if (*s == *accept)
-      len += how_many(&s, *accept);
-    accept++;
-  }
-  return len;
+	const char *start = s;
+
+    while (*s) {
+        if (!char_is_there(accept, *s)) break;
+		s++;
+    }
+    return (s - start);
 }
 
-//
+// #include <assert.h>
+// #include <stdio.h>
+// #include <string.h>
 // int main() {
-//   assert(strspn("hello_world", "hell") == my_strspn("hello_world", "hell"));
-//   assert(strspn("hello_world", "world") == my_strspn("hello_world", "world"));
-//   assert(strspn("hello_world", "hel") == my_strspn("hello_world", "hel"));
-//   assert(strspn("hello_world", "_") == my_strspn("hello_world", "_"));
-//   assert(strspn("hello_world", "    ") == my_strspn("hello_world", "    "));
-//   assert(strspn("hello_world", "hehabc") == my_strspn("hello_world", "hehabc"));
-//   assert(strspn("hello_world", "hello_banana_helloA") == my_strspn("hello_world", "hello_banana_helloA"));
-//   printf("all tests passed!\n");
+//   	assert(strspn("hello_world", "hell") == my_strspn("hello_world", "hell"));
+// 	assert(strspn("hello_world", "world") == my_strspn("hello_world", "world"));
+//   	assert(strspn("hello_world", "hel") == my_strspn("hello_world", "hel"));
+//   	assert(strspn("hello_world", "_") == my_strspn("hello_world", "_"));
+//   	assert(strspn("hello_world", "    ") == my_strspn("hello_world", "    "));
+//   	assert(strspn("hello_world", "hehabc") == my_strspn("hello_world", "hehabc"));
+//   	assert(strspn("hello_world", "hello_banana_helloA") == my_strspn("hello_world", "hello_banana_helloA"));
+//   	printf("all tests passed!\n");
 // }
