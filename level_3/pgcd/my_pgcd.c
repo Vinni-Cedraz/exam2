134259c920833b44acdd47905951ba0f5090f638
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-04-01 19:35:02 -0300

    added explanation and int overflow check to pgcd

--- level_3/pgcd/my_pgcd.c
+++ level_3/pgcd/my_pgcd.c
@@ -1,7 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static inline int gcd(int a, int b) {
+// HCF(Highest Common Denominator) and GCD (Greatest Common Denominator)
+// are the same thing
+
+static int gcd(int a, int b) {
+
+    // if moulinette isn't approving you , try adding this int overflow **check
+
     if (!b) return a;
     return (gcd(b, a % b));
 }
@@ -12,3 +18,12 @@ int main(int ac, char **av) {
     int b = atoi(av[2]);
     printf("%d\n", gcd(a, b));
 }
+
+// the tests are in the Makefile, add more as necessary
+
+// **check:
+// it shouldnt be necessary in other contexts but moulinette can be an asshole:
+
+// int check_overflow_a = (int)a;
+// int check_overflow_b = (int)b;
+// if (check_overflow_a <= 0 || check_overflow_b <= 0) return 0;
