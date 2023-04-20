c936c8854e60c6aa52220d759cabdda90682c87d
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-04-20 14:46:33 -0300

    made rot13 less confusing

--- level_1/rot_13/my_rot_13.c
+++ level_1/rot_13/my_rot_13.c
@@ -5,17 +5,17 @@
 static int is_lower(char c) { return (c <= 122 && c >= 97); }
 static int is_upper(char c) { return (c >= 65 && c <= 90); }
 static int my_putchar(char c) { return (write(1, &c, 1)); }
-static int alphabet_position(char c);
 static int is_alpha(char c) {
     return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
-static int is_beyond_end_of_alphabet(char c) {
-    return (alphabet_position(c) >= 14);
+
+// =============== MAIN FUNCTIONS, READ THEM FIRST ==========//
+
+static int position_in_alphabet(char c);
+static int is_beyond_middle_of_alphabet(char c) {
+    return (position_in_alphabet(c) > 13);
 }
-
-// =============== READ THE MAIN FUNCTIONS FIRST ==========//
-
-static void my_rot_13(char c);
+static void rot_13(char c);
 
 int main(int argc, char **argv) {
     if (argc != 2) return (my_putchar('\n'));
@@ -25,8 +25,8 @@ int main(int argc, char **argv) {
     while (str[i]) {
         if (!is_alpha(str[i]))
             my_putchar(str[i]);
-        else if (is_beyond_end_of_alphabet(str[i]))
-            my_rot_13(str[i]);
+        else if (is_beyond_middle_of_alphabet(str[i]))
+            rot_13(str[i]);
         else
             my_putchar(str[i] + 13);
         i++;
@@ -34,23 +34,25 @@ int main(int argc, char **argv) {
     return (my_putchar('\n'));
 }
 
-static void my_rot_13(char c) {
-    int rot13_alpha_pos;
-    int ascii_id;
+static void rot_13(char c) {
+    int alpha_pos_after_rot;
+    int ascii_position;
 
     if (is_lower(c)) {
-        rot13_alpha_pos = (alphabet_position(c) + 13) % 26;
-        ascii_id = rot13_alpha_pos + 96;
-        my_putchar(ascii_id);
+		// this next line is the main concept in the whole code
+        alpha_pos_after_rot = (position_in_alphabet(c) + 13) % 26;
+        ascii_position = alpha_pos_after_rot + 96;
+        my_putchar(ascii_position);
     }
     if (is_upper(c)) {
-        rot13_alpha_pos = (alphabet_position(c) + 13) % 26;
-        ascii_id = rot13_alpha_pos + 64;
-        my_putchar(ascii_id);
+		// this next line is the main concept in the whole code
+        alpha_pos_after_rot = (position_in_alphabet(c) + 13) % 26;
+        ascii_position = alpha_pos_after_rot + 64;
+        my_putchar(ascii_position);
     }
 }
 
-static int alphabet_position(char c) {
+static int position_in_alphabet(char c) {
     int i = 1;
     if (is_upper(c))
         i += c - 'A';
