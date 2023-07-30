c0074707b548956cc7245b1df91e2201e5e36e3f
vcedraz- <92558763+Vinni-Cedraz@users.noreply.github.com>
2023-07-30 19:21:36 +0000

    Update my_strcpy.c

--- level_1/ft_strcpy/my_strcpy.c
+++ level_1/ft_strcpy/my_strcpy.c
@@ -1,8 +1,9 @@
-char *my_strcpy(char *s1, char *s2) {
+char *ft_strcpy(char *s1, char *s2) {
     int i = -1;
     if (s1 && s2) {
-        while (*s2) *s1++ = *s2++;
-        *s1 = '\0';
+        while (s2[++i])
+            s1[i] = s2[i];
+        s1[i] = '\0';
     }
     return (s1);
 }
