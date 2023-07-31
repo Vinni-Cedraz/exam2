b19c1d62334a38c2b7ddb80f00a1d0c3bebce2ab
Vinni-Cedraz <Vinni.vcr@gmail.com>
2023-07-31 17:12:05 +0000

    fixed snake_to_camel's behavior when receiving multiple arguments

--- level_2/snake_to_camel/my_snake_to_camel.c
+++ level_2/snake_to_camel/my_snake_to_camel.c
@@ -1,23 +1,30 @@
 #include <unistd.h>
 
-static void my_putchar(char c);
-static void my_snake_to_camel(char *s);
-static void put_upper(char c, char **s);
+static int	my_putchar(char c);
+static void	my_snake_to_camel(char *s);
+static void	put_upper(char c, char **s);
 
-int main(int argc, char **argv) {
-    my_snake_to_camel(argv[1]);
-    my_putchar('\n');
+int	main(int argc, char **argv) {
+	if (argc != 2)
+		return (my_putchar('\n'));
+	my_snake_to_camel(argv[1]);
+	my_putchar('\n');
 }
 
-static  void my_snake_to_camel(char *s) {
-    while (s && *s) {
-        if (*s == '_') put_upper(*s, &s);
-        if (*s != '_') my_putchar(*s++);
-    }
+static void	my_snake_to_camel(char *s) {
+	while (s && *s)
+	{
+		if (*s == '_')
+			put_upper(*s, &s);
+		if (*s != '_')
+			my_putchar(*s++);
+	}
 }
 
-static  void my_putchar(char c) { write(1, &c, 1); }
+static int	my_putchar(char c) {
+	return(write(1, &c, 1));
+}
 
-static  void put_upper(char c, char **s) {
-    (*s)++, my_putchar(**s - 32), (*s)++;
+static void	put_upper(char c, char **s) {
+	(*s)++, my_putchar(**s - 32), (*s)++;
 }
