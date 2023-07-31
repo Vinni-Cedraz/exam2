f20d630af0cea970feb06d09fe3bc94d5b4f526a
Vinni-Cedraz <Vinni.vcr@gmail.com>
2023-07-31 17:16:36 +0000

    hidenp was printing an extra linebreak
    It was happening when it received the wrong number of args
    
    modified:   my_hidenp.c

--- level_2/max/my_max.c
+++ level_2/max/my_max.c
@@ -1,33 +1,48 @@
-int my_max(int arr[], int n) {
+#include <assert.h>
+static void print_arr(int *arr, unsigned int n);
+
+int my_max(int *arr, unsigned int n) {
     int max = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > max) max = arr[i];
-    }
+    for (int i = 1; i < n; i++)
+        if (arr[i] > max)
+            max = arr[i];
     return max;
 }
 
-int my_min(int arr[], int n) {
+int my_min(int *arr, unsigned int n) {
     int smallest = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (arr[i] < smallest) smallest = arr[i];
-    }
+    for (int i = 1; i < n; i++)
+        if (arr[i] < smallest)
+            smallest = arr[i];
     return smallest;
 }
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-int main() {
-    int arr[] = {1, 9, 2, 3, 4, -5, 7, 32, -45, 11, 87, 43, 6, 8};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int smallest = my_min(arr, n);
-	int biggest = my_max(arr, n);
-    int j = -1;
-
-    j = -1;
-    while (++j < n) printf("%d ", arr[j]);
-    printf("\n");
-    printf("\n");
-    printf("%d\n", my_min(arr, n));
-    printf("%d\n", my_max(arr, n));
-}
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
+// int main(void) {
+//     int static_arr[] = {1, 9, 2, 3, 4, -5, 7, 32, -45, 11, 87, 43, 6, 8};
+//
+//     int n1 = sizeof(static_arr);
+//     int n2 = sizeof(static_arr[0]);
+// 	int number_of_elements = n1/n2;
+//
+//     int smallest = my_min(static_arr, number_of_elements);
+//     int biggest = my_max(static_arr, number_of_elements);
+//
+//     printf("n1 => %d\n", n1);
+//     printf("n2 => %d\n", n2);
+//
+//     print_arr(static_arr, number_of_elements);
+//     assert(biggest == 87);
+//     assert(smallest == -45);
+//
+//     printf("all tests passed!\n");
+// }
+//
+// static void print_arr(int *arr, unsigned int n) {
+//     int j = -1;
+//     while (++j < n)
+//         printf("%d ", arr[j]);
+//     printf("\n");
+// }
--- level_3/hidenp/my_hidenp.c
+++ level_3/hidenp/my_hidenp.c
@@ -1,15 +1,16 @@
 #include <unistd.h>
 
-static  int my_putstr(char *s) {
-    while (s && *s) write(1, s++, 1);
-    write(1, "\n", 1);
+static int my_putstr(char *s) {
+    while (s && *s)
+        write(1, s++, 1);
     return 0;
 }
 
-static  int it_is_there(char c, char *s2) {
+static int it_is_there(char c, char *s2) {
     static int index;
     while (s2[index])
-        if (c == s2[index++]) return 1;
+        if (c == s2[index++])
+            return 1;
     return 0;
 }
 
@@ -20,7 +21,7 @@ int main(int argc, char **argv) {
     char *str1 = argv[1];
     while (*str1)
         if (!it_is_there(*str1++, argv[2])) {
-            return my_putstr("0");
+            return my_putstr("0\n");
         }
-    my_putstr("1");
+    my_putstr("1\n");
 }
