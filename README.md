9eb7eef0dccb2914d64db58ac4805946a43915be
vcedraz- <92558763+Vinni-Cedraz@users.noreply.github.com>
2023-08-19 14:40:44 -0300

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-These are my resolutions to the list of exercise done in preparation to the 42 exam tier 2.
+Here are my solutions to the list of exercises completed in preparation for tier 2 of the 42 exams.
+
+All of these solutions were tested and most of them were heavily tested. However, your ultimate shield against Moulinette is the subject and a big set of tests that **YOU UNDERSTAND**
+
+I openly accept and encourage you to open an issue if you find that one of my functions or programs does not pass a new and relevant test that you tried and can show me how to replicate the behavior.
+
+However, if you are going to complain about the fact that some functions do not pass the tests of the grademe.fr you need to have a good argument based on the subject. I say this because some of their tests are **not reasonable at all** and I even opened some issues at their repo about their tests for fprime, do_op, and maybe some others.
--- level_2/max/my_max.c
+++ level_2/max/my_max.c
@@ -3,7 +3,7 @@ static void print_arr(int *arr, unsigned int n);
 
 int my_max(int *arr, unsigned int n) {
     int max = arr[0];
-    for (int i = 1; i < n; i++)
+    for (unsigned int i = 1; i < n; i++)
         if (arr[i] > max)
             max = arr[i];
     return max;
@@ -11,7 +11,7 @@ int my_max(int *arr, unsigned int n) {
 
 int my_min(int *arr, unsigned int n) {
     int smallest = arr[0];
-    for (int i = 1; i < n; i++)
+    for (unsigned int i = 1; i < n; i++)
         if (arr[i] < smallest)
             smallest = arr[i];
     return smallest;
@@ -25,7 +25,7 @@ int my_min(int *arr, unsigned int n) {
 //
 //     int n1 = sizeof(static_arr);
 //     int n2 = sizeof(static_arr[0]);
-// 	int number_of_elements = n1/n2;
+//     int number_of_elements = n1 / n2;
 //
 //     int smallest = my_min(static_arr, number_of_elements);
 //     int biggest = my_max(static_arr, number_of_elements);
@@ -41,7 +41,7 @@ int my_min(int *arr, unsigned int n) {
 // }
 //
 // static void print_arr(int *arr, unsigned int n) {
-//     int j = -1;
+//     unsigned int j = -1;
 //     while (++j < n)
 //         printf("%d ", arr[j]);
 //     printf("\n");
